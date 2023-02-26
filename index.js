f7a56540a5196a168925ac5bdc11988ba9b56461
Eugene Park <e.park5336@gmail.com>
2023-02-26 17:53:35 -0500

    got adding a role working

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mysql = require('mysql2');
 const inquirer =require('inquirer');
 const cTable = require('console.table');
 const utilities = require('./utlilities/questionStorage.js');
-// var endProgram = false;
 require('dotenv').config();
 
 const db = mysql.createConnection(
@@ -16,7 +15,7 @@ console.log(`Connected to the database.`)
 )
 
 // function for view all departments
-function viewDept() {
+async function viewDept() {
   db.query('SELECT * FROM department', function (err, results) {
    if (err) {
      console.log(err)
@@ -30,7 +29,7 @@ function viewDept() {
  }
 // function for view all roles
 async function viewRoles() {
-  db.query('SELECT * FROM role', function (err, results) {
+  db.query('SELECT * FROM role JOIN department ON role.department_id = department.id', function (err, results) {
    if (err) {
      console.log(err)
    }
@@ -43,7 +42,7 @@ async function viewRoles() {
  }
 // function for view all employees
 async function viewEmployees() {
- db.query('SELECT * FROM employee', function (err, results) {
+ db.query('SELECT * FROM employee JOIN role ON employee.role_id = role.id', function (err, results) {
   if (err) {
     console.log(err)
   }
@@ -54,10 +53,74 @@ async function viewEmployees() {
   }
 }) 
 }
-// function for add a role
+// function for adding a department
+async function addDept() {
+  await inquirer.prompt(utilities.questionIndex.addDeptQ)
+  .then(async (response) => {
+    // console.log(response)
+    console.log('\n')
+    await db.promise().query(`
+    INSERT INTO department (dept_name)
+    VALUES ('${response.deptName}');`)
+    console.log('\n')
+  })
+  }
+
+
+// function for adding a role
+// helper function to gather list of departments
+// this function deconstructs the name and id data out of the sql data and rewrites into an array of strings
+
+var deptList
+var deptMapper
+async function deptFinder() {
+  let tempList = await db.promise().query('SELECT * FROM department')
+  // console.log(tempList)
+    deptList = tempList[0].map(({dept_name}) => (
+      `${dept_name}`
+    ))
+    // this makes an array to call the dept id later
+    deptMapper = tempList[0].map(({dept_name, id}) => (
+      {
+      departmentID: `${id}`,
+      departmentName: `${dept_name}`}))
+    // console.log(deptMapper);
+  }
+
+
 async function addRole() {
- await inquirer.prompt(utilities.questionIndex.addroleQ)
+  await inquirer.prompt([
+  {
+      type: "input",
+      name: "roleName",
+      Message: "what is the new role called?" 
+  },
+  {
+      type: "input",
+      name: "roleSalary",
+      Message: "what is the salary for this Role?" 
+  },
+  {
+      type: "list",
+      Message: "what Department is this role in?",
+      name: "roleDept",
+      choices: deptList
+  }
+])
+ .then(async (response) => {
+  // console.log(response);
+  let i = deptMapper.findIndex(x => 
+    x.departmentName === response.roleDept)
+  // console.log(i);
+  let deptID = deptMapper[i].departmentID;
+  await db.promise().query(`
+  INSERT INTO role (department_id, title, salary)
+  VALUES (${deptID}, ${response.roleName},${response.roleSalary});
+  `)
+ })
  }
+
+
 // function for add an employee
 async function addEmployee() {
   await inquirer.prompt(utilities.questionIndex.addEmployeeQ)
@@ -80,7 +143,11 @@ await inquirer.prompt(utilities.questionIndex.initialQ).then(async (response) =>
   else if (response.nextItem === 'View Employees') {
     await viewEmployees();
   }
+  else if (response.nextItem === 'add a department') {
+    await addDept();
+  }
   else if (response.nextItem === 'add a new role') {
+    await deptFinder();
     await addRole();
   }
   else if (response.nextItem === 'add a new employee') {
@@ -91,7 +158,9 @@ await inquirer.prompt(utilities.questionIndex.initialQ).then(async (response) =>
   }
   else if (response.nextItem === 'quit') {
     endProgram = true;
+    console.log('\n');
     console.log('byeee');
+    console.log('\n');
     process.exit();
   }
   else {
--- utlilities/questionStorage.js
+++ utlilities/questionStorage.js
@@ -1,22 +1,85 @@
 // question storage for reference with inquirer
+// const mysql = require('mysql2');
+
+// require('dotenv').config();
+
+// const db = mysql.createConnection(
+//   {
+//     host: `${process.env.db_host}`,
+//     user: `${process.env.user_name}`,
+//     password: `${process.env.sql_PW}`,
+//     database: `${process.env.db}`
+//   },
+//   )
+
+// const dept = async () => {
+//    let deptList = await db.promise().query('SELECT * FROM dept');
+//    return deptList
+// } 
+
+// console.log(dept());
+
 
 const questionIndex = {
     initialQ: [{
-        type: 'list',
         message: 'What would you like to do?',
+        type: 'list',
         name: 'nextItem',
         choices: [
         'View departments', 
         'View Roles', 
-        'View Employees', 
+        'View Employees',
+        'add a department', 
         'add a new role', 
         'add a new employee',
         'update an Employees Role',
         'quit']
     }],
-    addRollQ: [],
-    addEmployeeQ: [],
-    updateEmployeeRoleQ: [],
+    addDeptQ: [{
+        type: "input",
+        name: "deptName",
+        Message: "what is the name of the new department?"
+    }]
+    // addRoleQ: [
+    //     {
+    //         type: "input",
+    //         name: "roleName",
+    //         Message: "what is the new role called?" 
+    //     },
+    //     {
+    //         type: "input",
+    //         name: "roleSalary",
+    //         Message: "what is the salary for this Role?" 
+    //     },
+    //     {
+    //         type: "list",
+    //         Message: "what Department is this role in?",
+    //         name: "roleDept",
+    //         choices: ``
+    //     }
+//     addEmployeeQ: [
+//         {
+//             type: "input",
+//             name: "FirstName",
+//             Message: "what the new Employees First Name?" 
+//         },
+//         {
+//             type: "input",
+//             name: "lastName",
+//             Message: "what the new Employees last Name?" 
+//         },
+//         {
+//             type: "input",
+//             name: "employeeRole",
+//             Message: "what the new Employees role?" 
+//         },
+//         {
+//             type: "input",
+//             name: "employeeManager",
+//             Message: "who is the employees manager?" 
+//         },                      
+// ],
+//     updateEmployeeRoleQ: [],
 }
 
 module.exports = {questionIndex};
