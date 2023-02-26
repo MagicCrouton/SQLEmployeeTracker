461f8bde73283ce8b22bafbaa24758073a6fc5bc
Eugene Park <e.park5336@gmail.com>
2023-02-26 18:49:48 -0500

    got add employee functionality working

--- index.js
+++ index.js
@@ -115,16 +115,85 @@ async function addRole() {
   let deptID = deptMapper[i].departmentID;
   await db.promise().query(`
   INSERT INTO role (department_id, title, salary)
-  VALUES (${deptID}, ${response.roleName},${response.roleSalary});
+  VALUES (${deptID}, '${response.roleName}',${response.roleSalary});
   `)
  })
  }
 
+ var roleList
+ var roleMapper
+ var managerID
+ var employeeList
+//  var managerList
+// helper function for finding roles
+ async function roleFinder() {
+  let tempList = await db.promise().query('SELECT * FROM role')
+  // console.log(tempList)
+    roleList = tempList[0].map(({id, title, salary, deptartment_id}) => (
+      `${title}`
+    ))
+    // this makes an array to call the role id later
+    roleMapper = tempList[0].map(({id, title, salary, deptartment_id}) => (
+      {
+      roleID: `${id}`,
+      roleTitle: `${title}`,
+    }
+    ))
+    let tempEList = await db.promise().query('SELECT * FROM employee')
+      employeeList = tempEList[0].map(({id, role_id, first_name}) => (
+          `firstName: ${first_name} roleID: ${role_id}`
+      ))
+    // console.log(deptMapper);
+  }
+
 
 // function for add an employee
 async function addEmployee() {
-  await inquirer.prompt(utilities.questionIndex.addEmployeeQ)
+  await inquirer.prompt([
+  {
+      type: "input",
+      name: "firstName",
+      Message: "what is the new role called?" 
+  },
+  {
+      type: "input",
+      name: "lastName",
+      Message: "what is the salary for this Role?" 
+  },
+  {
+      type: "list",
+      Message: "what Department is this role in?",
+      name: "employeeRole",
+      choices: roleList
+  },
+  {
+    type: "list",
+    Message: "who is this employees manager?",
+    name: "employeeManager",
+    choices: employeeList
+  }
+])
+.then(async (response) => {
+ // console.log(response);
+ let i = roleMapper.findIndex(x => 
+   x.roleTitle === response.employeeRole)
+ console.log(i);
+ let roleID = roleMapper[i].roleID;
+ if (roleID-1 <= 1) {
+   managerID = 1;
+ }
+ else {
+   managerID = roleID - 1;
+ }
+ await db.promise().query(`
+ INSERT INTO employee (role_id, first_name, last_name, manager_id)
+ VALUES (${roleID},'${response.firstName}','${response.lastName}', ${managerID});
+ `)
+})
 }
+
+
+
 // function for update employee role
 async function updateEmployeeRole() {
   await inquirer.prompt(utilities.questionIndex.updateEmployeeRoleQ)
@@ -151,6 +220,7 @@ await inquirer.prompt(utilities.questionIndex.initialQ).then(async (response) =>
     await addRole();
   }
   else if (response.nextItem === 'add a new employee') {
+    await roleFinder();
     await addEmployee();
   }
   else if (response.nextItem === 'update an Employees Role') {
@@ -167,26 +237,5 @@ await inquirer.prompt(utilities.questionIndex.initialQ).then(async (response) =>
     console.log('you broke it, big sad :(');
   }
   })}
-
-  // const runProgram = async () => {
-  //   while(endProgram === false) {
-  //     choicePrompt();
-  //   }
-  // }
-// async function init(endProgram) {
-//     await choicePrompt();
-//     if (endProgram) {
-//       console.log('buh bye');
-//     }
-//     else {
-//       await init(endProgram);
-//     }
-//   };
-
-//   init(endProgram);
-
-// while(endProgram === false) async () => {
-//   await choicePrompt();
-// }
 
 runProgram()
